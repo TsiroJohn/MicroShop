a6331a9f2b53991e2ce0663de70eb95962f00e5d
TsiroJohn <thanos.tsirog@projectviewerp.com>
2022-05-17 22:15:09 +0300

    create CheckoutOrder, UpdateOrder commands based on mediator best practices

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/UpdateOrder/UpdateOrderCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.UpdateOrder
+{
+    public class UpdateOrderCommandHandler : IRequestHandler<UpdateOrderCommand>
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly IMapper mapper;
+        private readonly ILogger<UpdateOrderCommandHandler> logger;
+
+        public UpdateOrderCommandHandler(ILogger<UpdateOrderCommandHandler> logger, IOrderRepository orderRepository, IMapper mapper)
+        {
+            this.logger = logger;
+            this.orderRepository = orderRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToUpdate = await orderRepository.GetByIdAsync(request.Id);
+            if(orderToUpdate == null)
+            {
+                logger.LogError("Order not exist on database.");
+                //
+            }
+
+            mapper.Map(request, orderToUpdate, typeof(UpdateOrderCommand), typeof(Order));
+
+            await orderRepository.UpdateAsync(orderToUpdate);
+
+            logger.LogInformation($"Order {orderToUpdate.Id} is successfully updated.");
+
+            return Unit.Value;
+        }
+    }
+}
